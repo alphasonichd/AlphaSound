8f126f9297741cdd933f855f75a203cce21ae6d7
Alexander Snitko <alphasonichd@gmail.com>
2021-06-27 16:01:53 +0300

    Initial Commit

---
+++ AlphaSound/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlphaSound
+//
+//  Created by developer on 27.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = .red
+    }
+
+
+}
+
