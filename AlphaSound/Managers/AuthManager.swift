ae7b4b9c3b7e66f9d9379c812f4fed03aac566ce
Alexander Snitko <alphasonichd@gmail.com>
2021-07-04 17:12:59 +0300

    caching and refreshing tokens logic

--- AlphaSound/Managers/AuthManager.swift
+++ AlphaSound/Managers/AuthManager.swift
@@ -15,6 +15,8 @@ final class AuthManager {
         static let clientID = "eeb1f10962c7483c8f7b84a529164077"
         static let clientSecret = "09ba1e7e5865410d81fe093a3ee552d3"
         static let tokenAPIURL = "https://accounts.spotify.com/api/token"
+        static let redirectURI = "https://www.google.com"
+        static let scopes = "user-read-private%20playlist-modify-public%20playlist-read-private%20playlist-modify-private%20user-follow-read%20user-library-modify%20user-library-read%20user-read-email"
     }
     
     private init() {
@@ -22,31 +24,35 @@ final class AuthManager {
     }
     
     public var signInURL: URL? {
-        let scopes = "user-read-private"
         let base = "https://accounts.spotify.com/authorize"
-        let redirectURI = "https://www.google.com"
-        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(Constants.scopes)&redirect_uri=\(Constants.redirectURI)&show_dialog=TRUE"
         return URL(string: string)
     }
     
     var isSignedIn: Bool {
-        return false
+        return accessToken != nil
+//        return false
     }
     
     private var accessToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "access_token")
     }
     
     private var refreshToken: String? {
-        return nil
+        return UserDefaults.standard.string(forKey: "refresh_token")
     }
     
     private var tokenExpirationDate: Date? {
-        return nil
+        return UserDefaults.standard.object(forKey: "expirationDate") as? Date
     }
     
     private var shouldRefreshToken: Bool {
-        return false
+        guard let expirationDate = tokenExpirationDate else {
+            return false
+        }
+        let currentDate = Date()
+        let fiveMinutes: TimeInterval = 300
+        return currentDate.addingTimeInterval(fiveMinutes) >= expirationDate
     }
     
     public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
@@ -58,7 +64,7 @@ final class AuthManager {
         components.queryItems = [
             URLQueryItem(name: "grant_type", value: "authorization_code"),
             URLQueryItem(name: "code", value: code),
-            URLQueryItem(name: "redirect_uri", value: "https://www.google.com")
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI)
         ]
         
         var request = URLRequest(url: url)
@@ -73,27 +79,80 @@ final class AuthManager {
         request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
         request.httpBody = components.query?.data(using: .utf8)
         
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
             guard let data = data, error == nil else {
                 completion(false)
                 return
             }
             do {
-                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
-                print("SUCCESS: \(json)")
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                print(result)
+                self?.cacheToken(result: result)
+                completion(true)
             } catch {
                 completion(false)
             }
         }
         task.resume()
-        // Get Token
     }
     
-    private func cacheToken() {
+    private func cacheToken(result: AuthResponse) {
+        UserDefaults.standard.setValue(result.access_token, forKey: "access_token")
         
+        if let refreshToken = result.refresh_token {
+            UserDefaults.standard.setValue(refreshToken, forKey: "refresh_token")
+        }
+        
+        UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
+
     }
     
-    public func refreshAccessToken() {
+    public func refreshIfNeeded(completion: @escaping (Bool) -> Void) {
+//        guard shouldRefreshToken else {
+//            completion(true)
+//            return
+//        }
+        guard let refreshToken = refreshToken else {
+            return
+        }
         
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "refresh_token"),
+            URLQueryItem(name: "refresh_token", value: refreshToken)
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        let basicToken = Constants.clientID + ":" + Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            completion(false)
+            return
+        }
+        request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
+        request.httpBody = components.query?.data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            do {
+                let result = try JSONDecoder().decode(AuthResponse.self, from: data)
+                print("SUCCESFULLY REFRESHED!")
+                print(result)
+                self?.cacheToken(result: result)
+                completion(true)
+            } catch {
+                completion(false)
+            }
+        }
+        task.resume()
     }
 }
---
+++ AlphaSound/Models/AuthResponse.swift
@@ -0,0 +1,16 @@
+//
+//  AuthResponse.swift
+//  AlphaSound
+//
+//  Created by developer on 2.07.21.
+//
+
+import Foundation
+
+struct AuthResponse: Codable {
+    let access_token: String
+    let expires_in: Int
+    let refresh_token: String?
+    let scope: String
+    let token_type: String
+}
