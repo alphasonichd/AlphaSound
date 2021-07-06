f057f2e6811e97bf9e460b447e8726c1db6d3a7f
Alexander Snitko <alphasonichd@gmail.com>
2021-07-06 13:03:29 +0300

    getting profile data with generic request implementation

--- AlphaSound/Managers/APICaller.swift
+++ AlphaSound/Managers/APICaller.swift
@@ -12,9 +12,51 @@ final class APICaller {
     
     private init() {}
     
-    public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
+    struct Constants {
+        static let baseAPIURL = "https://api.spotify.com/v1"
+    }
+    
+    enum HTTPMethod: String {
+        case GET
+        case POST
+    }
+    
+    enum APIError: Error {
+        case failedToGetData
+    }
+    
+    private func createRequest(with url: URL?,
+                               type: HTTPMethod,
+                               completion: @escaping (URLRequest) -> Void) {
         AuthManager.shared.withValidToken { token in
-            
+            guard let apiURL = url else {
+                return
+            }
+            var request = URLRequest(url: apiURL)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
+            completion(request)
+        }
+    }
+    
+    public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
+        createRequest(with: URL(string: Constants.baseAPIURL + "/me"),
+                      type: .GET) { baseRequest in
+            let task = URLSession.shared.dataTask(with: baseRequest) { data, response, error in
+                guard let data = data, error == nil else {
+                    completion(.failure(APIError.failedToGetData))
+                    return
+                }
+                
+                do {
+                    let result = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    print(result)
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+            task.resume()
         }
     }
 }
