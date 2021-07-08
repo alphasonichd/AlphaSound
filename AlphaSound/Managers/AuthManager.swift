9e56b7e10c6c764feabcec699c533043f4d5a5de
Alexander Snitko <alphasonichd@gmail.com>
2021-07-08 23:12:32 +0300

    adding assets and launchscreen

--- AlphaSound/Managers/AuthManager.swift
+++ AlphaSound/Managers/AuthManager.swift
@@ -130,12 +130,12 @@ final class AuthManager {
         }
     }
     
-    public func refreshIfNeeded(completion: @escaping (Bool) -> Void) {
+    public func refreshIfNeeded(completion: ((Bool) -> Void)?) {
         guard !refreshingToken else {
             return
         }
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         guard let refreshToken = refreshToken else {
@@ -160,7 +160,7 @@ final class AuthManager {
         let basicToken = Constants.clientID + ":" + Constants.clientSecret
         let data = basicToken.data(using: .utf8)
         guard let base64String = data?.base64EncodedString() else {
-            completion(false)
+            completion?(false)
             return
         }
         request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
@@ -169,7 +169,7 @@ final class AuthManager {
         let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
             self?.refreshingToken = false
             guard let data = data, error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
             do {
@@ -179,9 +179,9 @@ final class AuthManager {
                 self?.cacheToken(result: result)
                 self?.onRefreshBlocks.forEach { $0(result.access_token) }
                 self?.onRefreshBlocks.removeAll()
-                completion(true)
+                completion?(true)
             } catch {
-                completion(false)
+                completion?(false)
             }
         }
         task.resume()
