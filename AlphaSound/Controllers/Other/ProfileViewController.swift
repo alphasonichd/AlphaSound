7d229a4916b4e3fde8c7f8b63a00e692048755d7
Alexander Snitko <alphasonichd@gmail.com>
2021-07-08 21:43:54 +0300

    Settings and Profile initial implementation

--- AlphaSound/Controllers/Other/ProfileViewController.swift
+++ AlphaSound/Controllers/Other/ProfileViewController.swift
@@ -8,19 +8,80 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private var models = [String]()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.isHidden = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
-        APICaller.shared.getCurrentUserProfile { result in
-            switch result {
-            case .success(let userProfileModel):
-                break
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchProfile()
+        view.backgroundColor = .systemBackground
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    private func fetchProfile() {
+        APICaller.shared.getCurrentUserProfile { [weak self] result in
+            DispatchQueue.main.async {
+//                self?.failedToGetProfile()
+                switch result {
+                case .success(let userProfileModel):
+                    self?.updateUI(with: userProfileModel)
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self?.failedToGetProfile()
+                }
+            }
+        }
+    }
+    
+    private func updateUI(with model: UserProfile) {
+        tableView.isHidden = false
+        models.append("Full Name: \(model.display_name)")
+        models.append("Email Address: \(model.email)")
+        models.append("User ID: \(model.id)")
+        models.append("Plan: \(model.product)")
+        // configure table models
+        tableView.reloadData()
+    }
+    
+    private func failedToGetProfile() {
+        let label = UILabel(frame: .zero)
+        label.text = "Faile to load profile."
+        label.sizeToFit()
+        label.textColor = .secondaryLabel
+        view.addSubview(label)
+        label.center = view.center
+    }
+    
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
     
 }
--- AlphaSound/Controllers/Other/SettingsViewController.swift
+++ AlphaSound/Controllers/Other/SettingsViewController.swift
@@ -14,9 +14,12 @@ class SettingsViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
+    
+    private var sections = [Section]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureModels()
         title = "Settings"
         view.backgroundColor = .systemBackground
         view.addSubview(tableView)
@@ -28,25 +31,57 @@ class SettingsViewController: UIViewController {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
     }
+    
+    private func configureModels() {
+        sections.append(Section(title: "Profile", options: [Option(title: "View Your Profile", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.viewProfile()
+            }
+        })]))
+        sections.append(Section(title: "Account", options: [Option(title: "Sign Out", handler: { [weak self] in
+            DispatchQueue.main.async {
+                self?.signOutTapped()
+            }
+        })]))
+    }
+    
+    private func viewProfile() {
+        let vc = ProfileViewController()
+        vc.title = "Profile"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func signOutTapped() {
+        
+    }
 }
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        1
+        sections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        sections[section].options.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = sections[indexPath.section].options[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "Foo"
+        cell.textLabel?.text = model.title
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         // Call handler for cell
+        let model = sections[indexPath.section].options[indexPath.row]
+        model.handler()
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let model = sections[section]
+        return model.title
     }
 }
