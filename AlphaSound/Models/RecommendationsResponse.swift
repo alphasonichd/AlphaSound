80cd3ce82458cda01bd3aa9f4428acb2f9decd91
Alexander Snitko <alphasonichd@gmail.com>
2021-07-11 22:26:32 +0300

    getting recommendations implementation

---
+++ AlphaSound/Models/RecommendationsResponse.swift
@@ -0,0 +1,13 @@
+//
+//  RecommendationsResponse.swift
+//  AlphaSound
+//
+//  Created by developer on 11.07.21.
+//
+
+import Foundation
+
+struct RecommendationsResponse: Codable {
+    let tracks: [AudioTrack]
+}
+
